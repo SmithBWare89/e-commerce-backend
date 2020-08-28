a445410927db3ebacc65e6bd4648f3d94627631a
SmithBWare <Smithwrestling89@gmail.com>
2020-08-28 11:50:01 -0400

    added nodemon

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,35 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+// Require all of the models
+const db = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req,res) => {
   // find all categories
   // be sure to include its associated Products
+  const catData = await db.Category.findAll({
+    include: [
+      {
+        model: db.Product
+      }
+    ]
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req,res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req,res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req,res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req,res) => {
   // delete a category by its `id` value
 });
 
